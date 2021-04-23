f4dba9182d115aadabfb89849ed7d12005030095
MIRIAM NAKI <nakiyingim@gmail.com>
2021-04-23 09:39:39 -0700

    modified knexfile

--- db/client.js
+++ db/client.js
@@ -1,6 +1,8 @@
 const knex = require('knex');
 const knexfile = require('../knexfile');
+const environment = process.env.DB_ENV || 'development';
 
-const client = knex(knexfile.development);
+
+const client = knex(knexfile[development,environment]);
 
 module.exports = client;
--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,15 @@
-// Update with your config settings.
+// // Update with your config settings.
 
 module.exports = {
 
   development: {
     client: 'pg',
     connection: {
-      host: '127.0.0.1',
+     
       database: 'cohort_db'
     
   },
+  
 
     migrations: {
       tableName: 'migrations',
@@ -17,6 +18,14 @@ module.exports = {
     seeds:{
       directory:'./db/seeds'
     }
-  }
+  },
+    production: {
+      client: 'pg',
+      connection: process.env.DATABASE_URL,
+      migrations: {
+          directory: './db/migrations',
+      },
+      seeds: { directory: './db/seeds' },
+    }
 
 };
