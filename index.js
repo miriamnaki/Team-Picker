67a06d7b5b81262c4d05fea3eaf63723deb21e54
MIRIAM NAKI <nakiyingim@gmail.com>
2021-04-22 20:49:47 -0700

    modified index.js

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ const knex = require('./db/client');
 // const PORT = 3000;
 const host = '0.0.0.0';
 const port = process.env.PORT || 3000;
-app.listen(host,port, () => {
+app.listen(port,host, () => {
   console.log(`Server listening on ......`);
 });
