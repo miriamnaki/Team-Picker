ed1ff3fbde1ab45b869b359a81fc0feb889827af
MIRIAM NAKI <nakiyingim@gmail.com>
2021-04-23 10:48:48 -0700

    modified knex filr

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,9 @@ module.exports = {
     production: {
       client: 'pg',
       connection: process.env.DATABASE_URL,
-      ssl:false,
+      ssl: {
+        rejectUnauthorized: false
+      },
       migrations: {
           directory: './db/migrations',
       },
